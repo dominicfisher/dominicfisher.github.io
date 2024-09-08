7d348d04e90dde44eb09d5ad4d81604f339ac468
dominicfisher <dominic.fisher@hotmail.com>
2024-09-07 22:58:07 -0500

    Added the spotify widget and made it look alot better

--- my-react-website/src/pages/Sidebar.js
+++ my-react-website/src/pages/Sidebar.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import './Sidebar.css';
+import SpotifyPlayer from './SpotifyPlayer';
 
 const Sidebar = () => {
     const [dropdownVisible, setDropdownVisible] = useState(false);
     const [profileDropdownVisible, setProfileDropdownVisible] = useState(false);
     const [ticketsDropdownVisible, setTicketsDropdownVisible] = useState(false);
     const [grindersDropdownVisible, setGrindersDropdownVisible] = useState(false);
+    const [sidebarOpen, setSidebarOpen] = useState(true);  // Sidebar is open by default
 
     const handleDropdownToggle = () => {
         setDropdownVisible(!dropdownVisible);
@@ -20,69 +21,91 @@ const Sidebar = () => {
     const handleTicketsDropdownToggle = () => {
         setTicketsDropdownVisible(!ticketsDropdownVisible);
     };
-    
+
     const handleGrindersDropdownToggle = () => {
         setGrindersDropdownVisible(!grindersDropdownVisible);
     };
 
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 768) {
+                setSidebarOpen(false);
+            } else {
+                setSidebarOpen(true);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Initial check
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <aside className="sidebar">
-        <nav className="nav-links">
-            <div id="logo">
-                <img src="/logo.png" alt="Company Logo" className="image"/>
-            </div>
-            <div>
-                <Link className="sidebar-button active" to="/"><i className='bx bx-home-alt-2'></i> Home</Link>
-                <div className="dropdown">
-                    <Link className="Slink" to="#" onClick={handleGrindersDropdownToggle}>
-                        <i className='bx bx-store-alt'></i> Grinders {grindersDropdownVisible ? '▲' : '▼'}
-                    </Link>
-                    <div className={`dropdown-container ${grindersDropdownVisible ? 'show' : ''}`}>
-                        <div className="dropdown-content">
-                            <Link className="dropdown-item" to="/grinders/c"><i className='bx bx-plus'></i> Open Orders</Link>
-                            <Link className="dropdown-item" to="/grinders/collection"><i className='bx bx-collection'></i> Awaiting Collection</Link>
-                            <Link className="dropdown-item" to="/grinders/OrderList"><i className='bx bx-notepad'></i> Current</Link>
-                            <Link className="dropdown-item" to="/grinders/history"><i className='bx bx-collection'></i> Past</Link>
-                        </div>
+        <>
+            {!sidebarOpen && <button className="open-btn" onClick={() => setSidebarOpen(true)}>Open Sidebar</button>}
+            <aside className={`sidebar ${sidebarOpen ? '' : 'closed'}`}>
+                <nav className="nav-links">
+                    <div id="logo">
+                        <img src="/logo.png" alt="Company Logo" className="image" />
                     </div>
-                </div>
-                
-                <div className="dropdown">
-                    <Link className="Slink" to="#" onClick={handleDropdownToggle}>
-                        <i className='bx bx-store-alt'></i> Orders {dropdownVisible ? '▲' : '▼'}
-                    </Link>
-                    <div className={`dropdown-container ${dropdownVisible ? 'show' : ''}`}>
-                        <div className="dropdown-content">
-                            <Link className="dropdown-item" to="/order/c"><i className='bx bx-plus'></i> Create</Link>
-                            <Link className="dropdown-item" to="/order/OrderList"><i className='bx bx-notepad'></i> Current</Link>
-                            <Link className="dropdown-item" to="/order/history"><i className='bx bx-collection'></i> Past</Link>
+                    <div>
+                        <Link className="sidebar-button active" to="/"><i className='bx bx-home-alt-2'></i> Home</Link>
+                        <div className="dropdown">
+                            <Link className="Slink" to="#" onClick={handleGrindersDropdownToggle}>
+                                <i className='bx bx-store-alt'></i> Grinders {grindersDropdownVisible ? '▲' : '▼'}
+                            </Link>
+                            <div className={`dropdown-container ${grindersDropdownVisible ? 'show' : ''}`}>
+                                <div className="dropdown-content">
+                                    <Link className="dropdown-item" to="/grinders/c"><i className='bx bx-plus'></i> Open Orders</Link>
+                                    <Link className="dropdown-item" to="/grinders/collection"><i className='bx bx-collection'></i> Awaiting Collection</Link>
+                                    <Link className="dropdown-item" to="/grinders/OrderList"><i className='bx bx-notepad'></i> Current</Link>
+                                    <Link className="dropdown-item" to="/grinders/history"><i className='bx bx-collection'></i> Past</Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                
-                <div className="dropdown">
-                    <Link className="Slink" to="#" onClick={handleTicketsDropdownToggle}>
-                        <i className='bx bx-support'></i> Tickets {ticketsDropdownVisible ? '▲' : '▼'}
-                    </Link>
-                    <div className={`dropdown-container ${ticketsDropdownVisible ? 'show' : ''}`}>
-                        <div className="dropdown-content">
-                            <Link className="dropdown-item" to="/staff/c"><i className='bx bx-plus'></i> Create</Link>
-                            <Link className="dropdown-item" to="/staff/history"><i className='bx bx-collection'></i> Past</Link>
+
+                        <div className="dropdown">
+                            <Link className="Slink" to="#" onClick={handleDropdownToggle}>
+                                <i className='bx bx-store-alt'></i> Orders {dropdownVisible ? '▲' : '▼'}
+                            </Link>
+                            <div className={`dropdown-container ${dropdownVisible ? 'show' : ''}`}>
+                                <div className="dropdown-content">
+                                    <Link className="dropdown-item" to="/order/c"><i className='bx bx-plus'></i> Create</Link>
+                                    <Link className="dropdown-item" to="/order/OrderList"><i className='bx bx-notepad'></i> Current</Link>
+                                    <Link className="dropdown-item" to="/order/history"><i className='bx bx-collection'></i> Past</Link>
+                                </div>
+                            </div>
                         </div>
+
+                        <div className="dropdown">
+                            <Link className="Slink" to="#" onClick={handleTicketsDropdownToggle}>
+                                <i className='bx bx-support'></i> Tickets {ticketsDropdownVisible ? '▲' : '▼'}
+                            </Link>
+                            <div className={`dropdown-container ${ticketsDropdownVisible ? 'show' : ''}`}>
+                                <div className="dropdown-content">
+                                    <Link className="dropdown-item" to="/staff/c"><i className='bx bx-plus'></i> Create</Link>
+                                    <Link className="dropdown-item" to="/staff/history"><i className='bx bx-collection'></i> Past</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <Link className="Slink" to="/faction"><i className='bx bxs-factory'></i> Faction</Link>
+                        <hr />
+                        <SpotifyPlayer />
                     </div>
-                </div>
-                <Link className="Slink" to="/faction"><i className='bx bxs-factory'></i> Faction</Link>
-                <hr />
-          
-                
-            </div>
-        </nav>
-        <ul className="icons2">
-            <li><a href="https://twitter.com/fisher_dominic" target="_blank" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="https://www.facebook.com/dominic.fisher.54" target="_blank" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-            <li><a href="mailto:dominic.fisher@hotmail.com" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
-        </ul>
-    </aside>
+                </nav>
+                <ul className="icons2">
+                    <li><a href="https://twitter.com/fisher_dominic" target="_blank" className="icon brands fa-twitter"><span className="label"><i class='bx bxl-twitter' ></i></span></a></li>
+                    <li><a href="https://www.facebook.com/dominic.fisher.54" target="_blank" className="icon brands fa-facebook-f"><span className="label"><i class='bx bxl-facebook' ></i></span></a></li>
+                    <li><a href="mailto:dominic.fisher@hotmail.com" className="icon solid fa-envelope"><span className="label"><i class='bx bx-envelope' ></i></span></a></li>
+                </ul>
+            </aside>
+            {sidebarOpen && <button className="close-btn" onClick={() => setSidebarOpen(false)}><i class='bx bx-menu' ></i></button>}
+        </>
     );
 };
 
---
+++ my-react-website/src/pages/SpotifyPlayer.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import './SpotifyPlayer.css';
+
+const SpotifyPlayer = () => {
+    const [songData, setSongData] = useState(null);
+    const [progressSeconds, setProgressSeconds] = useState(0);
+    const [totalSeconds, setTotalSeconds] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const serviceHost = "https://spotify.dominic-fisher.workers.dev/";
+    const spotifyUser = "Dominic";
+
+    useEffect(() => {
+        const updatePlayer = () => {
+            fetch(`${serviceHost}/get-now-playing`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.hasOwnProperty("ERROR")) {
+                        setSongData(null);
+                        return;
+                    }
+                    setSongData(data);
+                    if (data.item) {
+                        setProgressSeconds(Math.ceil(data.progress_ms / 1000));
+                        setTotalSeconds(Math.ceil(data.item.duration_ms / 1000));
+                        setIsPlaying(data.is_playing);
+                    }
+                });
+        };
+
+        const interval = setInterval(updatePlayer, 1000);
+        updatePlayer();
+
+        return () => clearInterval(interval);
+    }, [serviceHost]);
+
+    useEffect(() => {
+        if (isPlaying) {
+            const interval = setInterval(() => {
+                setProgressSeconds(prev => prev + 1);
+            }, 1000);
+            return () => clearInterval(interval);
+        }
+    }, [isPlaying]);
+
+    const pad = (num) => {
+        return num.toString().padStart(2, '0');
+    };
+
+    const progressWidth = (progressSeconds / totalSeconds) * 100;
+
+    return (
+        <div id="player" className="player">
+            <div className="row">
+                <div className="albumArtContainer">
+                    {songData && songData.item && songData.item.album && (
+                        <img id="player-album-art" src={songData.item.album.images[1].url} alt="Album Art" className="albumArt" />
+                    )}
+                </div>
+                <div className="infoContainer">
+                    <div id="player-song" className="songTitle">
+                        {songData && songData.item ? songData.item.name : `${spotifyUser} isn't playing anything.`}
+                    </div>
+                    <div id="player-artist" className="artistName">
+                        ~ {songData && songData.item && songData.item.artists ? songData.item.artists[0].name : ''}
+                    </div>
+                    {songData && songData.item && (
+                        <>
+                            <a id="player-song-link" href={songData.item.external_urls.spotify} target="_blank" rel="noopener noreferrer"></a>
+                            <a id="player-artist-link" href={songData.item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer" ></a>
+                            <a id="player-album-link" href={songData.item.album.external_urls.spotify} target="_blank" rel="noopener noreferrer" ></a>
+                            {songData.item.preview_url && <a id="player-mp3-link" href={songData.item.preview_url} target="_blank" rel="noopener noreferrer" ></a>}
+                        </>
+                    )}
+                    <div id="player-status" className="status">
+                        {isPlaying ? ` ${spotifyUser}'s now playing...` : ` ${spotifyUser} has paused.`}
+                    </div>
+                    <div id="player-time" className="time">
+                        {songData && songData.item && songData.item.duration_ms ? (
+                            <>
+                                {pad(Math.floor(progressSeconds / 60))}:{pad(progressSeconds % 60)} / {pad(Math.floor(totalSeconds / 60))}:{pad(totalSeconds % 60)}
+                            </>
+                        ) : null}
+                    </div>
+                    <div id="player-progress-back" className="progressBack">
+                        <div id="player-progress" className="progress" style={{ width: `${progressWidth}%` }}></div>
+                    </div>
+                   
+                </div>
+            </div>
+            <div id="player-background" className="background" style={{ backgroundImage: songData && songData.item && songData.item.album ? `url(${songData.item.album.images[1].url})` : 'none' }}></div>
+        </div>
+    );
+};
+
+export default SpotifyPlayer;
